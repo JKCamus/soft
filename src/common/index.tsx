989c098e5fb654c93b1776d107168d34f355d50f
Camus <924850758@qq.com>
2022-11-14 23:32:29 +0800

    feat: add form

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -1,4 +1,5 @@
-import { Input, Select } from "antd";
+import { Button, Checkbox, Form, Input,Select } from 'antd';
+
 import React from "react";
 
 const Common: React.FC = (props) => {
@@ -25,11 +26,41 @@ const Common: React.FC = (props) => {
   return (
     <div>
       <p>Common</p>
-      <Select defaultValue="麦秆" onChange={handleChange} options={map}></Select>
+      {/* <Select defaultValue="麦秆" onChange={handleChange} options={map}></Select>
       <Select defaultValue="梧桐木" onChange={handleChange} options={map2}></Select>
       <Select defaultValue="花生壳" onChange={handleChange} options={map3}></Select>
       <p>录入参数</p>
-      <Input placeholder="请输入参数" />
+      <Input placeholder="请输入参数" /> */}
+      <Form
+      name="basic"
+      wrapperCol={{ span: 8 }}
+      initialValues={{ remember: true }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="ωSiO2"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+
     </div>
   );
 };
